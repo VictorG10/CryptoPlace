42d93c9629440fb00d28764e5a24aff0a9fd3265
VictorG10 <victorgbadebo10@gmail.com>
2024-10-17 07:53:01 +0100

    updating cryptoplace

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./components/NavBar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import Coin from "./pages/Coin/Coin";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/coin/:coinId" element={<Coin />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./navbar.css";
 import logo_img from "../../assets/logo.png";
 import arrow from "../../assets/arrow_icon.png";
 import { Link } from "react-router-dom";
+import { CoinContext } from "../../contexts/CoinContext";
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  // const currencyHandler = (e) => {
+  //   switch (e.target.value) {
+  //     case "usd":
+  //       {
+  //         setCurrency((c) => (c = { name: "usd", symbol: "$" }));
+  //       }
+  //       break;
+  //     case "eur":
+  //       {
+  //         setCurrency((c) => (c = { name: "eur", symbol: "€" }));
+  //       }
+  //       break;
+  //     case "ngn":
+  //       {
+  //         setCurrency((c) => (c = { name: "ngn", symbol: "₦" }));
+  //       }
+  //       break;
+
+  //     default:
+  //       setCurrency((c) => (c = { name: "usd", symbol: "$" }));
+  //       break;
+  //   }
+  // };
+
+  const currencyHandler = (e) => {
+    const value = e.target.value;
+
+    setCurrency((c) =>
+      value === "usd"
+        ? { name: "usd", symbol: "$" }
+        : value === "eur"
+        ? { name: "eur", symbol: "€" }
+        : value === "ngn"
+        ? { name: "ngn", symbol: "₦" }
+        : { name: "usd", symbol: "$" }
+    );
+  };
+
   return (
     <div className="navbar">
       <Link to="/">
@@ -27,10 +68,10 @@ const Navbar = () => {
       </ul>
 
       <div className="nav-right">
-        <select>
+        <select onChange={(e) => currencyHandler(e)}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
-          <option value="naira">NGN</option>
+          <option value="ngn">NGN</option>
         </select>
         <button>
           Sign up <img className="btn-icon" src={arrow} alt="" />
